327ad1bd2619de70e2561c48a1cba7c7802c5a64
Virtual <nitaystain090@gmail.com>
2024-11-09 19:16:21 +0200

    returned handler_t

--- instruction.h
+++ instruction.h
@@ -7,10 +7,16 @@
 
 typedef void (*INSTRUCTION_HA)(cpu_t *, OPCODE *);
 
+typedef struct
+{
+    INSTRUCTION_HA handler;
+    size_t consumed_opcodes;
+} handler_t;
+
 void push(cpu_t *, OPCODE *);
 void pop(cpu_t *, OPCODE *);
 void inc_32_16(cpu_t *, OPCODE *);
 void dec_32_16(cpu_t *, OPCODE *);
-void mov_r32_16_imm(cpu_t*, OPCODE*);
+void mov_r32_16_imm(cpu_t *, OPCODE *);
 
 #endif
--- machine.c
+++ machine.c
@@ -4,27 +4,36 @@
 
 #include "map.h"
 
-int emulate_machine_code(cpu_t* cpu, OPCODE* opcodes, size_t opcodes_size) {
-    if (opcodes_size <= cpu->ip) {
+int emulate_machine_code(cpu_t *cpu, OPCODE *opcodes, size_t opcodes_size)
+{
+    if (opcodes_size <= cpu->ip)
+    {
         return 2;
     }
 
-    entry_t** entries_ptr = entries;
+    entry_t **entries_ptr = entries;
     DWORD start = cpu->ip;
-    for (;; cpu->ip++) {
-        entry_t* e = entries_ptr[opcodes[cpu->ip]];
-        if (e == NULL) {
+    for (;; cpu->ip++)
+    {
+        entry_t *e = entries_ptr[opcodes[cpu->ip]];
+        if (e == NULL)
+        {
             return 1;
         }
 
-        if (e->is_map) {
+        if (e->is_map)
+        {
             entries_ptr = e->ptr;
-            if (entries_ptr == NULL) {
+            if (entries_ptr == NULL)
+            {
                 return 1;
             }
-        } else {
-            handler_t* handler = (handler_t*)e->ptr;
-            if (opcodes_size - cpu->ip < handler->consumed_opcodes) {
+        }
+        else
+        {
+            handler_t *handler = (handler_t *)e->ptr;
+            if (opcodes_size - cpu->ip < handler->consumed_opcodes)
+            {
                 return 1;
             }
 
@@ -34,4 +43,3 @@ int emulate_machine_code(cpu_t* cpu, OPCODE* opcodes, size_t opcodes_size) {
         }
     }
 }
-
